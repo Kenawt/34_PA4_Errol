acd3f8890632a5c5c90629b957d8f746a0b22db4
Latha06 <64003410+Latha06@users.noreply.github.com>
2020-04-30 16:17:04 +0800

    Updated with MainScene-Latha

---
+++ 34_Errol_PA4/Assets/Scripts/NavigationScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class NavigationScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+        StartCoroutine(LoadScene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     IEnumerator LoadScene()
+    {
+
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("CubeScene");
+    }
+}
